c033c3d6b587aba736b88b32acb57d7522749778
KickButtowski80 <pazpaz22@yahoo.com>
2022-12-10 14:38:39 -0800

    formatting and got rid off unccessary console.logs

--- utils/params.js
+++ utils/params.js
@@ -1,12 +1,10 @@
 
 
-const validParams =(paramsQ) => {
-    console.log('here in params utils', paramsQ)
-    let {loc, unitGroup} = paramsQ
-    loc =  loc ||'los angeles'
-    unitGroup= unitGroup || 'us'
-    paramsQ = {...{loc, unitGroup}}
-    console.log('here in params utils', paramsQ)
+const validParams = (paramsQ) => {
+    let { loc, unitGroup } = paramsQ
+    loc = loc || 'los angeles'
+    unitGroup = unitGroup || 'us'
+    paramsQ = { ...{ loc, unitGroup } }
     return paramsQ
 }
 
