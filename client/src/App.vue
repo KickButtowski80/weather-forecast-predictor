1e3adb3ae4457f62e376e6092af9efd7e8c68188
KickButtowski80 <pazpaz22@yahoo.com>
2022-12-10 16:35:53 -0800

    add SearchBox cmp

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <main>
+    <search-box></search-box>
+    <hr>
     <weather-info></weather-info>
   </main>
 </template>
 
 <script>
 import WeatherInfo from "./components/WeatherInfo.vue";
+import SearchBox from "./components/SearchBox.vue";
 export default {
   components: {
     WeatherInfo,
+    SearchBox,
   },
 };
 </script>
---
+++ client/src/components/SearchBox.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Search:</h2>
+    <input type="text" name="" id="">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
