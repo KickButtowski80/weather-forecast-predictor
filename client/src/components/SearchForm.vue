65941a73b523d376a466fcedb928f0aa031e3c75
KickButtowski80 <pazpaz22@yahoo.com>
2022-12-10 19:54:50 -0800

    working on UI

--- client/src/components/SearchForm.vue
+++ client/src/components/SearchForm.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
+  <base-card>
     <input type="text" name="" id="" v-model="location" />
     <button @click="search">Search</button>
-  </div>
+  </base-card>
 </template>
 
 <script>
