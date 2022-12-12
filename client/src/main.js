deb7f510409f2a554e0d40db394bf0409b355d49
KickButtowski80 <pazpaz22@yahoo.com>
2022-12-12 12:00:07 -0800

    set up vue router

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 import BaseCard from './components/UI/BaseCard.vue'
 
 
 const app = createApp(App)
+app.use(VueRouter)
 app.component('base-card', BaseCard)
 app.mount('#app')
