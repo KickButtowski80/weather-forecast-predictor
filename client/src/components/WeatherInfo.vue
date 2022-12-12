932b3813b6ec9b4d201d3ee13f112f920f3c16b1
KickButtowski80 <pazpaz22@yahoo.com>
2022-12-11 20:53:44 -0800

    fixed undefiend issue from frontend

--- client/src/components/WeatherInfo.vue
+++ client/src/components/WeatherInfo.vue
@@ -41,7 +41,7 @@ export default {
   methods: {
     async fetchWeatherInfo() {
       const response = await fetch(
-        `http://localhost:4000/?loc=${this.location}`
+        `http://localhost:4000/?loc=${this.lookcriteria.location}&unit=${this.lookcriteria.unit}`
       );
       if (response.ok) {
         this.info = await response.json();
--- utils/params.js
+++ utils/params.js
@@ -2,11 +2,13 @@
 
 const validParams = (paramsQ) => {
     let { loc, unitGroup } = paramsQ
-    loc = loc || 'los angeles'
-    unitGroup = unitGroup || 'us'
+    
+    loc = (loc !== 'undefined' && loc)  || 'los angeles'
+    unitGroup = (unitGroup !== 'undefiend' && unitGroup)  || 'us'
     paramsQ = { ...{ loc, unitGroup } }
     return paramsQ
 }
+
 
 module.exports = {
     validParams
