3b6626cac88415353da0332e780e8d210d1d6645
KickButtowski80 <pazpaz22@yahoo.com>
2022-12-13 13:41:00 -0800

    formatting

--- db/CallExternalApiUsingHttp.js
+++ db/CallExternalApiUsingHttp.js
@@ -1,11 +1,12 @@
 const https = require("https");
 
 const result = (q, callback) => {
-    let { loc, unitGroup} = q
+    let { loc, unitGroup } = q
     let url =
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${loc}?unitGroup=${unitGroup}&key=QD7EHC4LQ3SCJCF9L99PMYPBA&contentType=json`
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${loc}?unitGroup=${unitGroup}&key=QD7EHC4LQ3SCJCF9L99PMYPBA&contentType=json`
     https.get(url, (req) => {
         let data = '';
+        
         req.on('data', (chunk) => {
             data += chunk
         })
@@ -13,7 +14,7 @@ const result = (q, callback) => {
         req.on('end', () => {
             return callback(data)
         })
-      console.info(`${url}`)
+        console.info(`${url}`)
     }).on('error', (err) => {
         console.log("Error:", err.message)
     })
