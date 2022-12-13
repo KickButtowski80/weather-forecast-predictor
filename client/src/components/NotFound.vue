4739b97f4d3cb8abe403f439c4cda5a63a38a043
KickButtowski80 <pazpaz22@yahoo.com>
2022-12-12 17:39:08 -0800

    added a gif to 404 page

--- client/src/components/NotFound.vue
+++ client/src/components/NotFound.vue
@@ -1,6 +1,7 @@
 <template>
    <h1>What U r looking for is not here</h1>
    <router-link to="/">Go back to Earth</router-link>
+   <img src="https://cdn.dribbble.com/users/219276/screenshots/3606924/astronaut.gif" alt="">
 </template>
 
 <script>
